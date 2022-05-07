bb346bfe01fb8634b449705f558ac3ccf9d464b6
Rachel Sidney Devianti <sidney.rachel@yahoo.co.id>
2022-05-08 07:21:43 +0900

    Pass config filename variable

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import sys
 from models.dependency_parser import DependencyParser
 from variables.config import init_config
 
 
 if __name__ == '__main__':
-    init_config('configs/test.json')
+    config_filename = sys.argv[1]
+    init_config(f'configs/{config_filename}.json')
 
     parser = DependencyParser()
     parser.train()
     parser.parse_sentence('The big dog lives in its little house.')
     parser.save_model()
 
-    parser = DependencyParser()
-    parser.load_model()
-    parser.parse_sentence('The big dog lives in its little house.')
+    # parser = DependencyParser()
+    # parser.load_model()
+    # parser.parse_sentence('The big dog lives in its little house.')
